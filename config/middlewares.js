06bd744773b2b6710cbbe54bb9cb138e133c08e4
mowais20 <m.owais1045@gmail.com>
2026-04-09 15:21:28 +0500

    images now showing in assets also

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,7 +1,18 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'img-src': ["'self'", 'data:', 'blob:', 'https://market-assets.strapi.io', 'https://taptapdevstorage.blob.core.windows.net'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
